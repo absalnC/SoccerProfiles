5c26cc6227b23a25d79314486a2d860f280c80f6
absa <absa.chemical007@gmail.com>
2014-11-08 19:30:07 -0600

    Lee datos de un solo jugador, los acomoda en un diccionario y lo agrega a una lista

--- jugadorespn.py
+++ jugadorespn.py
@@ -21,10 +21,10 @@ minJugadores=2654
 
 #for cuenta in range(minJugadores,2660):
  #   print cuenta
- #   r=requests.get(subUrl+str(cuenta))
- #   text=r.text.encode('utf-8').strip()
- #   soup=BeautifulSoup(text,"html5lib")
- #   name=soup.find("div",class_="profile")
+#r=requests.get(subUrl+str(cuenta))
+#text=r.text.encode('utf-8').strip()
+#soup=BeautifulSoup(text,"html5lib")
+#name=soup.find("div",class_="profile")
  #   items=  soup.find("ul",class_="profile-items")
  #   
    
@@ -42,11 +42,44 @@ text=r.text.encode('utf-8').strip()
 soup=BeautifulSoup(driver.page_source,"html5lib")
 #print soup.prettify
 stats=soup.find("div",id="ui-tabs-1")
-for child in stats.findAll('table'):
+for child in stats.findAll('table',):
     count =0   
-    for tr in child.findAll('tr'):
-         count+=1       
-         print tr.text
+    for tr in child.findAll('tr',{"class":lambda L:L!="colhead"}):
+        if count==0 :
+            dict["Temporada"]=tr.text.encode('utf-8'.strip())
+        else:
+            countb=0
+            for td in tr.findAll("td"):
+                if countb==0:
+                    dict["Equipo"]=td.text
+                if countb==1:
+                    dict["Competicion"]=td.text
+                if countb==2:
+                    dict["Iniciados"]=int(td.text)
+                if countb==3:
+                    dict["Bancas"]=int(td.text)
+                if countb==4:
+                    dict["Goles"]=int(td.text)
+                if countb==5:
+                    dict["Asistencias"]=int(td.text)
+                if countb==6:
+                    dict["Disaros"]=int(td.text)
+                if countb==7:
+                    dict["Al_arco"]=int(td.text)
+                if countb==8:
+                    dict["Cometidas"]=int(td.text)
+                if countb==9:
+                    dict["Recibidas"]=int(td.text)
+                if countb==10:
+                    dict["Amarillas"]=int(td.text)
+                if countb==11:
+                    dict["Rojas"]=int(td.text)
+                countb+=1
+            data.append(dict)
+        count+=1       
+    #print dict
+        
     #f.write(child.encode('utf-8'.strip()))
    
-    print count 
+    #print count 
+print data
